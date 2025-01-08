6b00b03fc9e089480bd80a4be31054e1ef5224be
Philip <f.baravi121@gmail.com>
2025-01-08 20:33:01 +0100

    Fix version missmatch between react and it's types

--- src/components/layout/header/logo.tsx
+++ src/components/layout/header/logo.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Delius_Swash_Caps } from "@next/font/google";
+import { Delius_Swash_Caps } from "next/font/google";
 import Link from "next/link";
 
 const deliusSwashCaps = Delius_Swash_Caps({
